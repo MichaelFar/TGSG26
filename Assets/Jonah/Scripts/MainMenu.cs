57ac71a801edd22a2a593c2302d6f65347dfa95b
LilNumpy <jonah2003chadwick@gmail.com>
2026-07-02 16:41:01 -0700

    Main Menu basic functions
    need to make options menu next

---
+++ Assets/Jonah/Scripts/MainMenu.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void NewGame()
+    {
+        Debug.Log("Starting New Game...");
+    }
+
+    public void LoadingOptions()
+    {
+        Debug.Log("Loading Options...");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quiting Game...");
+        Application.Quit();
+    }
+
+}
