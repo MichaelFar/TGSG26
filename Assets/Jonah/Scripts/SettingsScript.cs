b8df3bf464b693a5f35b0b68eeb7d31a17332f0a
LilNumpy <jonah2003chadwick@gmail.com>
2026-07-03 20:54:42 -0700

    Options menu works
    need to make the audio system work better

--- Assets/Jonah/Scripts/SettingsScript.cs
+++ Assets/Jonah/Scripts/SettingsScript.cs
@@ -2,10 +2,15 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.Rendering;
+using UnityEngine.UI;
 
 public class SettingsScript : MonoBehaviour
 {
+    //calling mouse look script for mouse sensitivity variable
+    public MoveCamera cameraSpeed;
+    public Slider sensitivitySlider;
 
+    //stuff for audio
     [SerializeField]
     private AudioMixer Mixer;
     [SerializeField]
@@ -13,6 +18,7 @@ public class SettingsScript : MonoBehaviour
     [SerializeField]
     private AudioMixMode MixMode;
 
+    //audio slider settings
     public void OnChangeSlider(float Value)
     {
         switch (MixMode)
@@ -22,17 +28,35 @@ public class SettingsScript : MonoBehaviour
                 break;
         }
     }
-    
-
     public enum AudioMixMode
     {
-        LinearAudioSourceVolume,
-        LinearMixerVolume,
         LogrithmicMixerVolume
     }
+
+    //mouse speed settings
+    public void Start()
+    {
+        cameraSpeed.mouseSensitivity = PlayerPrefs.GetFloat("currentSensitivity", 100);
+        sensitivitySlider.value = cameraSpeed.mouseSensitivity / 10;
+    }
+
+
+    public void Update()
+    {
+        PlayerPrefs.SetFloat("currentSensitivity", cameraSpeed.mouseSensitivity);
+    }
+
     
-    
-    
+   public void AdjustSpeed(float newSpeed)
+     {
+        cameraSpeed.mouseSensitivity = newSpeed * 10;
+     }
+
+
+
+
+
+
     /*
     public AudioMixer audioMixer;
     public void SetVolume(float volume)
