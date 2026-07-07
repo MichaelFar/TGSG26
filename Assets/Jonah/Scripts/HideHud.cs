d051d5df2504e57690164c0738d265367e34478b
LilNumpy <jonah2003chadwick@gmail.com>
2026-07-06 23:30:14 -0700

    Fixed all issues

--- Assets/Jonah/Scripts/HideHud.cs
+++ Assets/Jonah/Scripts/HideHud.cs
@@ -29,18 +29,20 @@ Date: 6/3/2026
         }
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    private void Start()
     {
-        JohnCanvasGroup = this.GetComponentInParent<CanvasGroup>();
+       ToggleHudVisibility(); 
+    }
+
+    void Awake()
+    {
+        JohnCanvasGroup = GetComponent<CanvasGroup>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        if (Input.GetKeyUp(KeyCode.Z))
-        {
-           ToggleHudVisibility();
-        }
+        
     }
 }
