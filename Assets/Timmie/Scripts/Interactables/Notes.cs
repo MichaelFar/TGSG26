df21985f5c8ce35cadead8ac8b2f17d17e62c06e
hairlineagod <timmiexyooj@gmail.com>
2026-07-11 00:24:05 -0700

    Debug sesh with Michael + Prefabs

--- Assets/Timmie/Scripts/Interactables/Notes.cs
+++ Assets/Timmie/Scripts/Interactables/Notes.cs
@@ -17,7 +17,7 @@ public class Notes : MonoBehaviour, IInteractable
     {
         UIHandler.Instance.ShowNoteUI(noteData);
         PauseMenu.Instance.SetGamePaused(true);
-        NoteInventory.Instance.AddNote(noteData);
-        Destroy(gameObject);
+        // NoteInventory.Instance.AddNote(noteData);
+        // Destroy(gameObject);
     }
 }
