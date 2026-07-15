19ed566dc9b178f26e0d85b1327b5d67e04b5153
MichaelFar <rarrafarrar@gmail.com>
2026-07-15 15:48:27 -0700

    Fixed normalization issue on player, fixed plants being terrible when picked up, fixed the deposit point for them, helped Kozeng create the rotation script for objects

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -9,6 +9,8 @@ public class MoveCamera : MonoBehaviour
 
     float xRotation = 0f;
 
+
+    private bool canRotate = true;
    // float smoothMouseX;
 
     //float smoothMod = 10.0f;
@@ -22,13 +24,23 @@ public class MoveCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        if(canRotate)
+        {
+            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-        //smoothMouseX = Mathf.Lerp(smoothMouseX, mouseX, smoothMod * Time.deltaTime);
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-        playerBody.Rotate(Vector3.up * mouseX);
+            xRotation -= mouseY;
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+            //smoothMouseX = Mathf.Lerp(smoothMouseX, mouseX, smoothMod * Time.deltaTime);
+            transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+            playerBody.Rotate(Vector3.up * mouseX);
+        }
+            
     }
+
+    public void SetCanRotate(bool new_value)
+    {
+        canRotate = new_value;
+    }
+
 }
