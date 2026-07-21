9874739e755799cb25a0ae900ce8e6e392ffa1b6
MageZoldyck <xdarkmage45x@gmail.com>
2026-07-20 23:04:24 -0700

    Fishing Suffering

--- Assets/Alec/FishingAction.cs
+++ Assets/Alec/FishingAction.cs
@@ -4,24 +4,59 @@ using UnityEngine.InputSystem;
 
 public class Fishing : MonoBehaviour
 {
-    public Canvas FishingQTE;
+    public CanvasGroup FishingQTE;
     public ItemPickup FishingRod;
+    public rotateIsActive RotateIsActive;
     public bool isHoldingFishingRod { get { return FishingRod.isHeld; } }
+    public bool isFishing = false;
+    public float deltaTimer = 0;
+    public float timeToHold = 4;
+    public bool timeRunning = false; 
 
     void Start()
     {
-        FishingQTE.enabled = false;
+        FishingQTE.alpha = 0;
     }
 
     void Update()
     {
         if (FishingRod.isHeld == true)
         {
-            //isHoldingFishingRod = true;
-            if (Input.GetMouseButtonDown(0))
+            if (timeRunning == true)
             {
-                FishingQTE.enabled = true;
+                deltaTimer += Time.deltaTime;
+
+                if (deltaTimer >= timeToHold)
+                {
+                    FishingQTE.alpha = 1;
+                    isFishing = true;
+                    timeRunning = false;
+                    deltaTimer = 0;
+                }
+            }
+
+            //isHoldingFishingRod = true;
+            if (RotateIsActive.canInputAgain == true)
+            {
+                if (Input.GetMouseButtonDown(0) && isFishing == false)
+                {
+                 print ("Started Fishing");
+                 timeRunning = true;
+                 RotateIsActive.canInputAgain = false;
+                }
+                else if (Input.GetMouseButtonDown(1) && isFishing == true)
+                {
+                StopFishing();
+                timeRunning = false;
+                deltaTimer = 0;
+                }
             }
         }
+    }
+
+    public void StopFishing()
+    {
+        FishingQTE.alpha = 0;
+        isFishing = false;
     }
 }
--- Assets/Alec/QuickTimeEvent.cs
+++ Assets/Alec/QuickTimeEvent.cs
@@ -1,39 +1,97 @@
 using UnityEngine;
+using UnityEngine.Events;
 
-public class Rotate : MonoBehaviour
+public class rotateIsActive : MonoBehaviour
 {
-    public Fishing isFishing;
+    public Fishing FishingActivated;
     public float spinSpeed = 20;
+    public float startLocation = 0;
     public float currentLocation = 0;
+    public float minimumThreshold = 235;
+    public float maximumThreshold = 275;
+    
+    public int finishedMinigameTimer = 0;
+    public bool finishedMinigame = false;
+    [HideInInspector]
+    public bool canInputAgain = true;
+
+    public UnityEvent onSuccess;
+    public UnityEvent onFailure;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-    
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (finishedMinigameTimer >= 1)
+        {
+            FishingActivated.StopFishing();
+            canInputAgain = true;
+            finishedMinigameTimer = 0;
+            finishedMinigame = false;
+        }
+        
+        if (finishedMinigame == true)
+        {
+            finishedMinigameTimer += 1;
+            canInputAgain = false;
+        }
 
-        if (isFishing.FishingQTE == true)
+        if (FishingActivated.isFishing)
         {
             transform.Rotate(Vector3.forward * spinSpeed * Time.deltaTime);
-            currentLocation = transform.localEulerAngles.z;
-            
+            currentLocation = transform.localEulerAngles.z;    
+
             if (Input.GetMouseButtonDown(0))
-            {
-                if (currentLocation > 235 && currentLocation < 275)
                 {
-                    print("Success");
-                    isFishing.FishingQTE.enabled = false;
-                }
-                else
-                {
-                    print("Fail");
-                    isFishing.FishingQTE.enabled = false;
-                }
+                    if (currentLocation > minimumThreshold && currentLocation < maximumThreshold)
+                    {
+                        //print("Success");
+                        onSuccess.Invoke();
+                        transform.rotation = Quaternion.Euler(0, 0, startLocation);
+                        finishedMinigame = true;
+
+                    }
+                    else
+                    {
+                        //print("Fail");
+                        onFailure.Invoke();
+                        transform.rotation = Quaternion.Euler(0, 0, startLocation);
+                        finishedMinigame = true;
+                        
+                    }
                
-            }
+                }
         }
+        else
+        {
+            transform.rotation = Quaternion.Euler(0, 0, startLocation);
+        }
+        
+        
+        /* if (isFishing.FishingQTE == true)
+         {
+            transform.Rotate(Vector3.forward * spinSpeed * Time.deltaTime);
+            currentLocation = transform.localEulerAngles.z;    
+            if (Input.GetMouseButtonDown(0))
+                {
+                    if (currentLocation > 235 && currentLocation < 275)
+                     {
+                        print("Success");
+                        
+                    }
+                    else
+                    {
+                        print("Fail");
+                        
+                    }
+               
+                }
+        }
+        */
     }
 }
